f032e93fb4f3b3a3f7976d6dfb3df4484e6ac3e0
kaway404 <32229419+kaway404@users.noreply.github.com>
2017-12-04 19:32:54 -0200

    Arrumado

--- buscaanime.php
+++ buscaanime.php
@@ -19,7 +19,7 @@ else
  ?>
  <?php
  $videoid = $animel['id'];
-$videols2 = DBRead( 'videos', "WHERE id and idserie = '". $videoid ."' ORDER BY id DESC LIMIT 1" );
+$videols2 = DBRead( 'videos', "WHERE id and idserie = '". $videoid ."' ORDER BY id ASC LIMIT 1" );
  if (!$videols2)
 	echo "";
 else 
