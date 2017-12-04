b3168639ac425dc5ee51f93276f606cbf9f3d2d5
kaway404 <32229419+kaway404@users.noreply.github.com>
2017-12-04 20:14:22 -0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1584,21 +1584,21 @@ else
 </style>
 <?php
 require 'db.php';
-$totaldepost = mysql_query("SELECT * FROM netflix_series WHERE id ");
+$totaldepost = mysql_query("SELECT * FROM netflix_series WHERE id and views >= 1 ");
 $totaldepost = mysql_num_rows($totaldepost);
 if($totaldepost >= 6){
 ?>
-<div class="left-i" onclick="ScrollByRight5()">
+<div class="left-i" onclick="ScrollByRight7()">
 <svg enable-background="new 0 0 32 32" height="32px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M7.701,14.276l9.586-9.585c0.879-0.878,2.317-0.878,3.195,0l0.801,0.8c0.878,0.877,0.878,2.316,0,3.194  L13.968,16l7.315,7.315c0.878,0.878,0.878,2.317,0,3.194l-0.801,0.8c-0.878,0.879-2.316,0.879-3.195,0l-9.586-9.587  C7.229,17.252,7.02,16.62,7.054,16C7.02,15.38,7.229,14.748,7.701,14.276z" fill="#fff"/></svg>
 </div>
 
-<div class="right-i" onclick="ScrollByLeft5()">
+<div class="right-i" onclick="ScrollByLeft7()">
 <svg enable-background="new 0 0 32 32" height="32px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M24.291,14.276L14.705,4.69c-0.878-0.878-2.317-0.878-3.195,0l-0.8,0.8c-0.878,0.877-0.878,2.316,0,3.194  L18.024,16l-7.315,7.315c-0.878,0.878-0.878,2.317,0,3.194l0.8,0.8c0.878,0.879,2.317,0.879,3.195,0l9.586-9.587  c0.472-0.471,0.682-1.103,0.647-1.723C24.973,15.38,24.763,14.748,24.291,14.276z" fill="#fff"/></svg>
 </div>
 
 <?php } ?>
 
-<div class="video-a" id="scroll5">
+<div class="video-a" id="scroll7">
 
 <?php
 $animels2 = DBRead( 'series', "WHERE id and views >= 1 ORDER BY id DESC LIMIT 40" );
@@ -1749,7 +1749,7 @@ else
 </style>
 <?php
 require 'db.php';
-$totaldepost = mysql_query("SELECT * FROM netflix_series WHERE id ");
+$totaldepost = mysql_query("SELECT * FROM netflix_series WHERE id <> '". $batata ."' ");
 $totaldepost = mysql_num_rows($totaldepost);
 if($totaldepost >= 6){
 ?>
@@ -1947,6 +1947,20 @@ function ScrollLeft() {
 }
 function ScrollRight() {
 	document.getElementById("scroll5").scrollLeft=-1500;
+}
+
+function ScrollByLeft7() {
+	document.getElementById("scroll7").scrollBy(1500, 0);
+}
+function ScrollByRight7() {
+	document.getElementById("scroll7").scrollBy(-1500, 0);
+}
+
+function ScrollLeft() {
+	document.getElementById("scroll7").scrollLeft=1500;
+}
+function ScrollRight() {
+	document.getElementById("scroll7").scrollLeft=-1500;
 }
 
 
