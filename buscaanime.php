b5cce7f36f5271f60df7c0b6a034055079875ff6
kaway404 <32229419+kaway404@users.noreply.github.com>
2017-12-05 17:58:38 -0200

    Arrumado busca

--- buscaanime.php
+++ buscaanime.php
@@ -11,7 +11,7 @@ if(empty($nome)){
 	echo "";
 }else{?>
 <?php
-$animels2 = DBRead( 'series', "WHERE name LIKE '%$nome%' ORDER BY id DESC LIMIT 15" );
+$animels2 = DBRead( 'series', "WHERE name LIKE '%$nome%' ORDER BY id DESC LIMIT 9" );
  if (!$animels2)
 	echo "";
 else 
@@ -26,7 +26,16 @@ else
 	foreach ($videols2 as $videol):
  ?>
  
- <a class="eoq" href='/watch.php?id=<?php echo $videol['id']; ?>'><li class='buscaright'><img src='<?php echo $animel['foto']; ?>' class='avatar6'/><div class='name'><?php echo $animel['name']; ?></div></li></a>
+ <a class="eoq" href='/watch.php?id=<?php echo $videol['id']; ?>'><li class='buscaright'><img src='<?php echo $animel['foto']; ?>' class='avatar6'/><div class='name'>
+ <?php
+	$str2 = nl2br( $animel['name'] );
+	$len2 = strlen( $str2 );
+	$max2 = 25;
+   if( $len2 <= $max2 )
+   echo $str2;
+	else    
+   echo substr( $str2, 0, $max2 ) . '...'?>
+ </div></li></a>
  
  <?php endforeach; endforeach;?>
 <?php }?>
