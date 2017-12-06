cb2bd156f4f65f1263788dca97f3dd4d7f78469b
kaway404 <32229419+kaway404@users.noreply.github.com>
2017-12-06 11:05:52 -0200

    Arrumado style e logo

--- configure.php
+++ configure.php
@@ -26,10 +26,10 @@ if(isset($_COOKIE['iduser']) and (isset($_COOKIE['inisession']))){
 
 <head>
 <title>
-Netflix
+Wooby
 </title>
 <meta charset="UTF-8">
-<link rel="shortcut icon" href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.ico"/>
+<link rel="shortcut icon" href="/static/ico/default.ico"/>
 </head>
 
 <?php 
@@ -111,15 +111,15 @@ body {
 }
 
 
-.header-al-home-a svg{
+.header-al-home-a .logo{
 		height: 3vw;
-		margin-top: 1.1vw;
+		margin-top: 0.5vw;
 		margin-left: 3vw;
 		opacity: .8;
 		cursor: pointer;
 	}
 
-    .header-al-home-a svg:hover{
+    .header-al-home-a .logo:hover{
         opacity: 1;
 	}
 </style>
@@ -128,7 +128,7 @@ body {
 <body style="background: #141414;">
 
 <div class="header-al-home-a">
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 81.387"><g fill="#b81d24"><path d="M256.09 76.212c4.178.405 8.354.84 12.52 1.29l9.198-22.712 8.743 24.807c4.486.562 8.97 1.152 13.44 1.768l-15.328-43.501L299.996 0H287.01l-.135.186-8.283 20.455L271.32.003h-12.822l13.237 37.565-15.644 38.644zM246.393 75.322V0h-12.817v74.265c4.275.33 8.552.684 12.817 1.056M150.113 71.11c3.46 0 6.916.026 10.366.054V43.492h15.397V31.708H160.48v-19.91h17.733V0h-30.6v71.12c.831 0 1.666-.013 2.5-.01M110.319 71.83c4.27-.152 8.544-.28 12.824-.384V11.8h11.98V.003H98.339V11.8h11.982v60.03h-.002zM12.295 79.772V34.897L27.471 77.96c4.667-.524 9.341-1.017 14.028-1.483V.001H29.201v46.483L12.825.001H0v81.384h.077c4.063-.562 8.14-1.096 12.218-1.613M85.98 11.797V.001H55.377V75.202a1100.584 1100.584 0 0 1 30.578-2.211V61.184c-5.916.344-11.82.74-17.71 1.181V43.497h15.397V31.706H68.245V11.797H85.98zM203.614 60.62V-.003h-12.873v71.876c10.24.376 20.44.9 30.606 1.56V61.619c-5.9-.381-11.81-.712-17.733-1"/></g></svg>
+<img src="/static/logo.png" class="logo"/>
 </div>
 
 <style>
@@ -170,7 +170,7 @@ body {
                 box-shadow: 0 1px 1px rgba(0,0,0,.25);
                 color: #fff;
                 width: 30vw;
-                background: #e50914;
+                background: #5e09e5;
                 font-size: 1.5vw;
             }
 </style>
