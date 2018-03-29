62fcbb817d485688049b796cce7b75752dcd1e2e
Alexandre Silva <32229419+kaway404@users.noreply.github.com>
2018-03-29 19:18:25 -0300

    Fix close button

--- request.php
+++ request.php
@@ -9,18 +9,27 @@ if($idse){
 	echo '<script>location.href="dashboard";</script>';	
 	}
 	if(isset($_GET['serie'])){
+	$infor = $_GET['infor'];
 		?>
-<div class="informa">
 
 <script>
   $('#close').click(function(){
-	$("#infor").fadeOut(600);
+	$("#infor<?php echo $infor;?>").fadeOut(600);
     });
 </script>
 
-<div class="close" id="close" style="z-index: 10421421312321;">
+<div class="informa">
+
+
+<div class="close" id="close<?php echo $infor;?>" style="z-index: 10421421312321;">
 	<svg height="2vw"  width:"2vw" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z"/></svg>
 </div>
+
+<script>
+  $('#close<?php echo $infor;?>').click(function(){
+	$("#infor<?php echo $infor;?>").fadeOut(600);
+    });
+</script>
 
 <style>
 .left-de{
@@ -98,11 +107,11 @@ $totaldepost = mysql_query("SELECT * FROM netflix_videos WHERE idserie = '{$idwa
 $totaldepost = mysql_num_rows($totaldepost);
 if($totaldepost >= 6){
 ?>
-<div class="left-i" onclick="ScrollByRight()" style="position: relative; top: 7vw;">
+<div class="left-i" onclick="ScrollByRight()" style="position: relative; top: 9.5vw; left: 2.2vw;">
 <svg enable-background="new 0 0 32 32" height="32px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M7.701,14.276l9.586-9.585c0.879-0.878,2.317-0.878,3.195,0l0.801,0.8c0.878,0.877,0.878,2.316,0,3.194  L13.968,16l7.315,7.315c0.878,0.878,0.878,2.317,0,3.194l-0.801,0.8c-0.878,0.879-2.316,0.879-3.195,0l-9.586-9.587  C7.229,17.252,7.02,16.62,7.054,16C7.02,15.38,7.229,14.748,7.701,14.276z" fill="#fff"/></svg>
 </div>
 
-<div class="right-i" onclick="ScrollByLeft()" style="position: relative; top: 7vw;">
+<div class="right-i" onclick="ScrollByLeft()" style="position: relative; top: 9.5vw;">
 <svg enable-background="new 0 0 32 32" height="32px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M24.291,14.276L14.705,4.69c-0.878-0.878-2.317-0.878-3.195,0l-0.8,0.8c-0.878,0.877-0.878,2.316,0,3.194  L18.024,16l-7.315,7.315c-0.878,0.878-0.878,2.317,0,3.194l0.8,0.8c0.878,0.879,2.317,0.879,3.195,0l9.586-9.587  c0.472-0.471,0.682-1.103,0.647-1.723C24.973,15.38,24.763,14.748,24.291,14.276z" fill="#fff"/></svg>
 </div>
 <?php } ?>
@@ -134,6 +143,12 @@ foreach ($resultsearchs as $resultsearch):
 </a>
 
 	<?php endforeach; endforeach; ?>
+
+<script>
+  $('#close').click(function(){
+	$("#infor<?php echo $infor;?>").fadeOut(600);
+    });
+</script>
 
 </div>
 </div>
@@ -242,5 +257,12 @@ function ScrollRight() {
 </div>
 
 </div>
+
+
+<script>
+  $('#close').click(function(){
+	$("#infor<?php echo $infor;?>").fadeOut(600);
+    });
+</script>
 
 	<?php } ?>
